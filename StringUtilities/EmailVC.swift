b433bd289bb38ad0d38bedd7363b1654d2dc031c
Mihaylov Artem <mag@napoleonit.ru>
2017-10-30 10:47:11 +0500

    continuing demo

---
+++ StringUtilities/EmailVC.swift
@@ -0,0 +1,30 @@
+//
+//  EmailVC.swift
+//  StringExtensions
+//
+//  Created by Artem Mihaylov on 28.10.2017.
+//  Copyright © 2017 Artem Mihaylov. All rights reserved.
+//
+
+import UIKit
+
+class EmailVC: UIViewController {
+    
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func checkButtonTapped() {
+        guard let email = emailTF.text, !email.characters.isEmpty else {
+            resultLabel.text = "Enter e-mail"
+            resultLabel.textColor = .red
+            return
+        }
+        if email.isCorrectEmail() {
+            resultLabel.text = "Correct e-mail"
+            resultLabel.textColor = UIColor.green
+        } else {
+            resultLabel.text = "Incorrect e-mail"
+            resultLabel.textColor = .red
+        }
+    }
+}
---
+++ StringUtilities/PhoneVC.swift
@@ -0,0 +1,22 @@
+//
+//  PhoneVC.swift
+//  StringExt
+//
+//  Created by Артем Михайлов on 28.10.2017.
+//  Copyright © 2017 Артем Михайлов. All rights reserved.
+//
+
+import UIKit
+
+class PhoneVC: UIViewController {
+    
+    @IBOutlet weak var formatTF: UITextField!
+    @IBOutlet weak var textTF: UITextField!
+    @IBOutlet weak var formattedTextTF: UITextField!
+    @IBOutlet weak var defaultFormatL: UILabel!
+    
+    @IBAction func standartFormatPhoneButtonTapped() {
+        
+    }
+    
+}
